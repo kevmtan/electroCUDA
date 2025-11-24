6c250734cf357fd8e883abebfcb32f529045f7a5
Kevin Tan <kevmtan@gmail.com>
2025-11-24 01:42:40 -0600

    mustBeTextOrNumeric

--- src/admin/mustBeTextOrNumeric.m
+++ src/admin/mustBeTextOrNumeric.m
@@ -1,5 +1,9 @@
 function mustBeTextOrNumeric(input)
-    if ~(istext(input) || isnumeric(input))
-        error('Input must be text or numeric');
-    end
+% This function is part of the electroCUDA package:
+% https://kevmtan.github.io/electroCUDA
+%
+% Kevin Tan, 2025
+
+if ~(istext(input) || isnumeric(input))
+    error('Input must be text or numeric');
 end
