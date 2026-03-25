7955c802633d2f14eeba171067e047064a579d38
Kevin Tan <kevmtan@gmail.com>
2026-03-24 20:50:22 -0500

    clean up subject list on config scripts

--- user/pipeline_KT/multivariate/do_classifySpecROI.m
+++ user/pipeline_KT/multivariate/do_classifySpecROI.m
@@ -1,9 +1,9 @@
-sbjs = ["S12_33_DA";"S12_34_TC";"S12_35_LM";"S12_36_SrS";"S12_38_LK";"S12_39_RT";"S12_40_MJ";...
-    "S12_41_KS";"S12_42_NC";"S12_45_LR";"S13_46_JDB";"S13_47_JT2";"S13_50_LGM";...
-    "S13_51_MTL";"S13_52_FVV";"S13_53_KS2";"S13_54_KDH";"S13_56_THS";"S13_57_TVD";...
-    "S13_59_SRR";"S13_60_DY";"S14_62_JW";"S14_66_CZ";"S14_67_RH";"S14_74_OD";...
-    "S14_75_TB";"S14_76_AA";"S14_78_RS";"S15_81_RM";"S15_82_JB";"S15_83_RR";...
-    "S15_87_RL";"S16_95_JOB";"S16_96_LF"];
+sbjs = ["S12_33_DA";"S12_34_TC";"S12_35_LM";"S12_36_SrS";"S12_38_LK";"S12_39_RT";...
+    "S12_40_MJ";"S12_41_KS";"S12_42_NC";"S12_45_LR";"S13_46_JDB";"S13_47_JT2";...
+    "S13_50_LGM";"S13_51_MTL";"S13_52_FVV";"S13_53_KS2";"S13_54_KDH";"S13_56_THS";...
+    "S13_57_TVD";"S13_59_SRR";"S13_60_DY";"S14_62_JW";"S14_66_CZ";"S14_67_RH";...
+    "S14_74_OD";"S14_75_TB";"S14_76_AA";"S14_78_RS";"S15_81_RM";"S15_82_JB";...
+    "S15_83_RR";"S15_87_RL";"S16_95_JOB";"S16_96_LF"];
 %sbjs = ["S12_38_LK";"S12_42_NC"];
 proj = "lbcn";
 task = "MMR"; % task name
@@ -49,12 +49,6 @@ o.psyVars = ["frame" "latency" "pct" "RT" "resp" "valence"]; % psy vars to inclu
 o.condVar = "cond";
 o.cond = ["Semantic" "Episodic"]; % Conditions to classify (train & test)
 o.condx = ["Self" "Other"];       % Conditions to cross-classify (predict)
-
-% Outliers within timepoint / data split
-o.ol = "median"; % Outlier detection method (mathworks.com/help/stats/filloutlier.html)
-o.olFill = "clip"; % Outlier fill method
-o.olThrAll = 0; % All-observation outlier threshold (0=skip)
-o.olThrCond = 3; % Within-condition outlier threshold (0=skip)
 
 % Rank calculation & PCA
 o.pca = "roi"; % Run rank calculation & PCA by ["ch"|"roi"|"split"](channel,ROI,analysis data split)
@@ -118,7 +112,7 @@ end
 if isequal(o.fun,@fitcsvm)
     o.OptimizeHyperparameters = "BoxConstraint"; % "BoxConstraint" "KernelScale"
 elseif isequal(o.fun,@fitclinear)
-    o.OptimizeHyperparameters = "Lambda"; % "Lambda" "Learner"
+    o.OptimizeHyperparameters = "none"; % "Lambda" "Learner"
 elseif isequal(o.fun,@fitcdiscr)
     o.OptimizeHyperparameters = "Gamma"; % "Gamma" "Delta"
 elseif isequal(o.fun,@fitcknn)
@@ -140,7 +134,7 @@ o.epoch.badTrials = ""; % Bad trial removal criteria
 % Epoch time limits (secs) [nan=variable, 0=none]
 o.epoch.pre = nan; % Duration before stim onset [nan = pre-stim ITI]
 o.epoch.post = nan; % Duration after stim offset [nan = post-stim ITI]
-o.epoch.dur = nan; % Duration after stim onset, supercedes 'post' [nan = no limit]
+o.epoch.dur = 2.025; % Duration after stim onset, supercedes 'post' [nan = no limit]
 % Epoch time bins
 o.epoch.bin = 0.025; % latency bin width (secs)
 o.epoch.binPct = 5; % latency percentage bin width (<=100)
